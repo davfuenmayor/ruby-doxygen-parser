4a60bfb425ee1e9571e96c424e73642d49822ff1
David <david@david.(none)>
2013-03-17 20:14:21 +0100

    Fixes

--- doxyparser-2.2.gem_FILES/data/lib/nodes/enum.rb
+++
@@ -1,8 +0,0 @@
-class DoxyEnum < DoxyMember
-   
-   def values
-     ret=[]
-     xpath("enumvalue/name").each{ |v| ret << v.child.content}
-     ret
-   end
-end
--- doxyparser-2.2.gem_FILES/data/lib/nodes/variable.rb
+++
@@ -1,3 +0,0 @@
-class DoxyVariable < DoxyMember
-   
-end
