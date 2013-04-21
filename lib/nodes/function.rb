c615a42fc5ac261089a39e7c44fd7bfce9faa174
David <david@david.(none)>
2013-04-21 20:03:47 +0200

    Fix getter for void

--- lib/nodes/function.rb
+++ lib/nodes/function.rb
@@ -13,15 +13,15 @@ module Doxyparser
     end
 
     def getter_for
-      if @args == '()'
+      if @params.empty? || (@params.size == 1 && @params[0].type =~ /\s*void\s*/)
         if @basename.start_with?('get') || @basename.start_with?('Get')
-          return @basename.gsub(/get[_]?(\w)/i){|match| $1.downcase} 
-        else 
+          return @basename.gsub(/get[_]?(\w)/i){|match| $1.downcase}
+        else
           return nil
         end
         if @basename.start_with?('is') || @basename.start_with?('Is')
-          return @basename.gsub(/is[_]?(\w)/i){|match| $1.downcase} 
-        else 
+          return @basename.gsub(/is[_]?(\w)/i){|match| $1.downcase}
+        else
           return nil
         end
       else
@@ -34,7 +34,7 @@ module Doxyparser
       if @type == 'void'
         if @basename.start_with?('set') || @basename.start_with?('Set')
           return @basename.gsub(/set[_]?(\w)/i){|match| $1.downcase}
-        else 
+        else
           return nil
         end
       else
@@ -54,11 +54,11 @@ module Doxyparser
       all_params.each { |param|
         decl_name= param.xpath("declname")
         if decl_name == nil || decl_name.empty? || decl_name[0].child==nil
-           decl_name = ""
-         else
-           decl_name = decl_name[0]
+          decl_name = ""
+        else
+        decl_name = decl_name[0]
         end
-        @params << Param.new(find_type(param),decl_name)  
+        @params << Param.new(find_type(param),decl_name)
       }
     end
   end
